6e4ced8286ff6ba2e6608b5ef476b3084e3cffdc
Alim Tegar Wicaksono <42492971+alimtegar@users.noreply.github.com>
2023-08-10 14:07:32 +0700

    Update push_to_hf.py

--- push_to_hf.py
+++ push_to_hf.py
@@ -4,7 +4,7 @@ from huggingface_hub import HfApi
 api = HfApi()
 api.upload_folder(
     folder_path='./lora-alpaca/',
-    repo_id='alimtegar/webgen-2-lora',
+    repo_id='alimtegar/webgen-7b-lora',
     allow_patterns='*',
     delete_patterns='*',
 )
